bd985ca1ffc1cc22cbc8397f0200972f67d22f0a
ABBOU Antoine <aabbou@g-a101-13.local>
2017-09-27 18:21:51 +0200

    Initial commit - Base and structure

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <h1 v-if="store.user.name"> Bienvenue {{ store.user.name }} </h1>
+    <router-view :store="store" @login="onLogin"></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      store: {
+        user: {}, // user informations
+        users: ['Franquito', 'Paquito', 'Pepito'], // users list
+        messages: ['Hello', 'Bonjour'] // messages list
+      }
+    }
+  },
+  methods: {
+    onLogin (username) {
+      this.store.user = {
+        name: username
+        // avatar
+      }
+    }
+  }
+}
+</script>
+
+<style lang="stylus">
+@import "styles/theme"
+
+html
+  +below(600px) // rupture
+    font-size 50%
+
+</style>
---
+++ src/components/views/Login.vue
@@ -0,0 +1,40 @@
+<template>
+  <main>
+    <form @submit.prevent = "onSubmit">
+      <p class="error" v-if="error"> The username is invalid</p>
+      <input v-model="username" type="text" placeholder="username">
+
+      <button> Login </button>
+    </form>
+  </main>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        error: false,
+        username: ''
+      }
+    },
+    methods: {
+      onSubmit (e) {
+        // validation
+        // TODO : Vérifier que le username n'existe pas déjà
+        if (!this.username.match(/^\w{1,15}$/)) {
+          console.log(this.username)
+          this.error = true
+        } else {
+          this.error = false
+          this.$emit('login', this.username)
+          this.$router.push('/')
+        }
+      }
+    }
+  }
+</script>
+
+<style lang="stylus" scoped>
+  .error
+    color red
+</style>
